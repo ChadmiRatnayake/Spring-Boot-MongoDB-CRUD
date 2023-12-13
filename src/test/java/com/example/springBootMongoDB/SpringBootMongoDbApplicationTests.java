b429d50a444e157c3ba230e703be14b8f2817c01
ChadmiRatnayake <chadmi.20@cse.mrt.ac.lk>
2023-12-14 00:03:24 +0530

    Initial commit

---
+++ src/test/java/com/example/springBootMongoDB/SpringBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
